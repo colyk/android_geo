741d3cff8b9dee04781219b3d3af4224f2f73cf1
colykxer <colykxer@gmail.com>
2019-11-29 04:31:20 +0100

    Add fingerprint login

--- app/src/main/java/colyk/geo/MainActivity.java
+++ app/src/main/java/colyk/geo/MainActivity.java
@@ -1,233 +1,76 @@
 package colyk.geo;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
+import android.content.Intent;
+import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
+import com.multidots.fingerprintauth.AuthErrorCodes;
+import com.multidots.fingerprintauth.FingerPrintAuthCallback;
+import com.multidots.fingerprintauth.FingerPrintAuthHelper;
 
-import org.osmdroid.events.DelayedMapListener;
-import org.osmdroid.events.MapListener;
-import org.osmdroid.events.ScrollEvent;
-import org.osmdroid.events.ZoomEvent;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.compass.CompassOverlay;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+public class MainActivity extends AppCompatActivity implements FingerPrintAuthCallback {
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-
-    private static final int MULTIPLE_PERMISSION_REQUEST_CODE = 4;
-    private MapView mapView;
-    private Location mLastLocation;
-    private GoogleApiClient mGoogleApiClient;
+    private FingerPrintAuthHelper mFingerPrintAuthHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mFingerPrintAuthHelper = FingerPrintAuthHelper.getHelper(this, this);
+        mFingerPrintAuthHelper.startAuth();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-        }
-
-        checkPermissionsState();
-    }
-
-    private void checkPermissionsState() {
-        int internetPermissionCheck = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.INTERNET);
-
-        int networkStatePermissionCheck = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_NETWORK_STATE);
-
-        int writeExternalStoragePermissionCheck = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-        int coarseLocationPermissionCheck = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION);
-
-        int fineLocationPermissionCheck = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION);
-
-        int wifiStatePermissionCheck = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_WIFI_STATE);
-
-        if (internetPermissionCheck == PackageManager.PERMISSION_GRANTED &&
-                networkStatePermissionCheck == PackageManager.PERMISSION_GRANTED &&
-                writeExternalStoragePermissionCheck == PackageManager.PERMISSION_GRANTED &&
-                coarseLocationPermissionCheck == PackageManager.PERMISSION_GRANTED &&
-                fineLocationPermissionCheck == PackageManager.PERMISSION_GRANTED &&
-                wifiStatePermissionCheck == PackageManager.PERMISSION_GRANTED) {
-
-            setupMap();
-
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{
-                            Manifest.permission.INTERNET,
-                            Manifest.permission.ACCESS_NETWORK_STATE,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                            Manifest.permission.ACCESS_COARSE_LOCATION,
-                            Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_WIFI_STATE},
-                    MULTIPLE_PERMISSION_REQUEST_CODE);
-        }
+        setContentView(R.layout.fingerprint);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MULTIPLE_PERMISSION_REQUEST_CODE: {
-                if (grantResults.length > 0) {
-                    boolean somePermissionWasDenied = false;
-                    for (int result : grantResults) {
-                        if (result == PackageManager.PERMISSION_DENIED) {
-                            somePermissionWasDenied = true;
-                        }
-                    }
-                    if (somePermissionWasDenied) {
-                        Toast.makeText(this, "Cant load maps without all the permissions granted", Toast.LENGTH_SHORT).show();
-                    } else {
-                        setupMap();
-                    }
-                } else {
-                    Toast.makeText(this, "Cant load maps without all the permissions granted", Toast.LENGTH_SHORT).show();
-                }
-                return;
-            }
-
-        }
-    }
-
-    private void setupMap() {
-
-        mapView = (MapView) findViewById(R.id.mapview);
-        mapView.setClickable(true);
-        mapView.setBuiltInZoomControls(true);
-        //setContentView(mapView); //displaying the MapView
-
-        mapView.getController().setZoom(15); //set initial zoom-level, depends on your need
-        //mapView.getController().setCenter(ONCATIVO);
-        //mapView.setUseDataConnection(false); //keeps the mapView from loading online tiles using network connection.
-        mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
-
-        MyLocationNewOverlay oMapLocationOverlay = new MyLocationNewOverlay(getApplicationContext(), mapView);
-        mapView.getOverlays().add(oMapLocationOverlay);
-        oMapLocationOverlay.enableFollowLocation();
-        oMapLocationOverlay.enableMyLocation();
-        oMapLocationOverlay.enableFollowLocation();
-
-        CompassOverlay compassOverlay = new CompassOverlay(this, mapView);
-        compassOverlay.enableCompass();
-        mapView.getOverlays().add(compassOverlay);
-
-        mapView.setMapListener(new DelayedMapListener(new MapListener() {
-            public boolean onZoom(final ZoomEvent e) {
-                MapView mapView = (MapView) findViewById(R.id.mapview);
-
-                String latitudeStr = "" + mapView.getMapCenter().getLatitude();
-                String longitudeStr = "" + mapView.getMapCenter().getLongitude();
-
-                String latitudeFormattedStr = latitudeStr.substring(0, Math.min(latitudeStr.length(), 7));
-                String longitudeFormattedStr = longitudeStr.substring(0, Math.min(longitudeStr.length(), 7));
-
-                Log.i("zoom", "" + mapView.getMapCenter().getLatitude() + ", " + mapView.getMapCenter().getLongitude());
-                TextView latLongTv = (TextView) findViewById(R.id.textView);
-                latLongTv.setText("" + latitudeFormattedStr + ", " + longitudeFormattedStr);
-                return true;
-            }
-
-            public boolean onScroll(final ScrollEvent e) {
-                MapView mapView = (MapView) findViewById(R.id.mapview);
-
-                String latitudeStr = "" + mapView.getMapCenter().getLatitude();
-                String longitudeStr = "" + mapView.getMapCenter().getLongitude();
-
-                String latitudeFormattedStr = latitudeStr.substring(0, Math.min(latitudeStr.length(), 7));
-                String longitudeFormattedStr = longitudeStr.substring(0, Math.min(longitudeStr.length(), 7));
-
-                Log.i("scroll", "" + mapView.getMapCenter().getLatitude() + ", " + mapView.getMapCenter().getLongitude());
-                TextView latLongTv = (TextView) findViewById(R.id.textView);
-                latLongTv.setText("" + latitudeFormattedStr + ", " + longitudeFormattedStr);
-                return true;
-            }
-        }, 1000));
-    }
-
-    protected void onStart() {
-        mGoogleApiClient.connect();
-        super.onStart();
-    }
-
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
-
-    private void setCenterInMyCurrentLocation() {
-        if (mLastLocation != null) {
-            mapView.getController().setCenter(new GeoPoint(mLastLocation.getLatitude(), mLastLocation.getLongitude()));
-        } else {
-            Toast.makeText(this, "Getting current location", Toast.LENGTH_SHORT).show();
-        }
+    protected void onResume() {
+        super.onResume();
+        //start finger print authentication
+        mFingerPrintAuthHelper.startAuth();
     }
 
     @Override
-    public void onConnected(Bundle connectionHint) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+    public void onNoFingerPrintHardwareFound() {
+        //Device does not have finger print scanner.
+        Toast.makeText(this, "no Cool", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
-    public void onConnectionSuspended(int i) {
+    public void onNoFingerPrintRegistered() {
+        //There are no finger prints registered on this device.
+        Toast.makeText(this, "no Cool", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+    public void onBelowMarshmallow() {
+        //Device running below API 23 version of android that does not support finger print authentication.
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onAuthSuccess(FingerprintManager.CryptoObject cryptoObject) {
+        //Authentication sucessful.
+        Toast.makeText(this, "Cool", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, MapActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        } else if (id == R.id.action_locate) {
-            setCenterInMyCurrentLocation();
+    public void onAuthFailed(int errorCode, String errorMessage) {
+        switch (errorCode) {    //Parse the error code for recoverable/non recoverable error.
+            case AuthErrorCodes.CANNOT_RECOGNIZE_ERROR:
+                //Cannot recognize the fingerprint scanned.
+                break;
+            case AuthErrorCodes.NON_RECOVERABLE_ERROR:
+                //This is not recoverable error. Try other options for user authentication. like pin, password.
+                break;
+            case AuthErrorCodes.RECOVERABLE_ERROR:
+                //Any recoverable error. Display message to the user.
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
